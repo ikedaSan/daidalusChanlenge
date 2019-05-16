875465ce60f271229144440892002ec92889afda
ikedaSan <ferperja@gmail.com>
2019-05-16 23:22:58 +0200

    data to JSON

--- daidalus.py
+++ daidalus.py
@@ -4,13 +4,9 @@ from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-    return '<h1>Hello World</h1>'
 
-@app.route('/user/<name>')
-def user(name):
-    return '<h1>Hello, {}!</h1>'.format(name)
+
+
 
 @app.route('/export', methods=['GET'])
 def export_data():
@@ -21,10 +17,9 @@ def export_data():
 
     #return  df_gen16.to_html();
 
-    return df_gen16.nlargest(5,'GENNTAN',keep='all').to_html()
+    return df_gen16.nlargest(5,'GENNTAN',keep='all').to_json(orient="records")
 
-#        to_json(orient="records")
-#        df_gen16.to_html()
+
 
 
 
@@ -36,11 +31,7 @@ def dataByState(filter):
     url ="https://www.epa.gov/sites/production/files/2018-02/egrid2016_data.xlsx"
     columnas = 'A,B' + ',' + filter
     df_st16 = pd.read_excel(url_file,sheet_name='ST16',header=1,usecols=columnas)
-    return df_st16.to_html()
-#return df_st16.to_json(orient="records")
-
-#excel.make_response_from_array([[1, 2], [3, 4]], "xlsx",
-#                                         file_name="../egrid2016_data")
+    return df_st16.to_json(orient="records")
 
 
 if __name__ == '__main__':
